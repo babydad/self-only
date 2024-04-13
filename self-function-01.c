920045c4e0d20baf2612a9e5e8edbd61f8eecae7
babydad <babydad2002@hotmail.com>
2024-04-13 17:09:01 +0800

    Update self-function-01.c

--- self-function-01.c
+++ self-function-01.c
@@ -1,11 +1,18 @@
-int main(int argv,int *argc)
+//#include <stdio.h>
+/**
+ * 程序主入口函数
+ * @param argc 参数个数
+ * @param argv 参数值数组
+ * @return 返回程序执行状态，0代表正常结束
+ */
+int main(int argc,int *argv[])
 {
-  int i;
-  int j;
-  for(i=0,i++,i<10)
-  {
-    j=1;
-    j+=j;
+  // 初始化变量j为1
+  int j = 1;
+  // 循环遍历10次，每次将j递增1
+  for(int i=0;i<10;i++){
+    j += 1;
   }
-  return j;
+  // 循环结束后，程序正常退出
+  return 0;
 }
